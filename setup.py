aa66a1355cf792134e043728ad031db56332c3ad
bjoerndot <bjoerndot@gmail.com>
2021-05-02 17:41:56 +0200

    bumped version to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="simple_python_couch_db",
-    version="0.0.2",
+    version="0.1.0",
     author="Oliver Schwamb",
     author_email="mail@oliverschwamb.de",
     description="Simple adapter to use CouchDB with Python.",
